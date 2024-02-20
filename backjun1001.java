047e0b1c078f90890cc77d30b36928e17ec4603d
gyu in <tlarb2022@naver.com>
2024-02-20 20:24:40 +0900

    1002

--- backjun1001.java
+++ backjun1001.java
@@ -1,12 +1,12 @@
 import java.util.Scanner;
 
-public class backjun1000 {
+public class backjun1001 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
 		int A = in.nextInt();
 		int B = in.nextInt();
 		
-		System.out.println(A+B);
+		System.out.println(A-B);
  
 		in.close();
 	}
