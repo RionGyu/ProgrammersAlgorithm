b1eb8be6277dc0359ed9fe7031259d251b65f918
gyu in <tlarb2022@naver.com>
2024-02-21 22:03:43 +0900

    연습

--- backjun10869.java
+++ backjun10869.java
@@ -1,16 +1,15 @@
 import java.util.Scanner;
 
-public class backjun1001 {
+public class backjun10869 {
+    
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
 		int A = in.nextInt();
 		int B = in.nextInt();
-		
-		in.close();
+
         System.out.println(A+B);
         System.out.println(A-B);
-        System.out.println(A*B);
-        System.out.println(A/B);
-        System.out.println(A%B);
-	}  
+ 
+		in.close();
+    }
 }
