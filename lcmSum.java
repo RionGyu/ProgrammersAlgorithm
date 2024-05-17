c2faa67b02229cd6678d987e0c586b257d27aa0f
gyu in <tlarb2022@naver.com>
2024-05-17 18:50:51 +0900

    lcmsum

--- lcmSum.java
+++ lcmSum.java
@@ -1,20 +1,31 @@
-public class lcmSum {
+class lcmSum {
+    public int solution(int[] arr) {
+        int answer = 0;
 
-    public static int gcd(int x, int y) {
-        while (y != 0) {
-            int temp = y;
-            y = x % y;
-            x = temp;
+        if(arr.length == 1) return arr[0]; //원소가 한 개인 경우 바로 출력
+
+        int g = gcd(arr[0], arr[1]); //처음 두 원소의 최대공약수
+        answer = (arr[0] * arr[1]) / g; //처음 두 원소의 최소공배수
+
+        /*  
+        원소의 개수가 3개 이상인 경우
+        앞의 두 원소의 최소 공배수와 다음 원소의 최소 공배수를 구하며
+        배열의 끝까지 반복
+        */ 
+        if(arr.length > 2) { 
+            for(int i = 2; i < arr.length; i++) {
+                g = gcd(answer, arr[i]);
+                answer = (answer * arr[i]) / g;
+            }
         }
-        return x;
+
+        return answer;
     }
 
-    
-    public static int lcm(int x, int y) {
-        return x * (y / gcd(x, y));
+    //최대 공약수
+    private static int gcd(int a, int b) {
+        int r = a % b;
+        if(r == 0) return b;
+        else return gcd(b, r);
     }
-
-
-
-    
 }
