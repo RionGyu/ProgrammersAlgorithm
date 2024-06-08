aa8d6336161cd36bc952ee7acdac8f46a3176dbc
gyu in <tlarb2022@naver.com>
2024-06-08 20:03:36 +0900

    commit test

--- javascriptTest.js
+++ javascriptTest.js
@@ -4,6 +4,7 @@ const rl = readline.createInterface({
     output: process.stdout
 });
 
+
 let input = [];
 
 rl.on('line', function (line) {
---
+++ javascriptTest2.js
@@ -0,0 +1,12 @@
+a = [1,3,45,2,10]
+a.forEach((e, i) =>{
+    console.log(e,i)
+})
+
+const str = "Hello World";
+const ret = str.split("")
+console.log(ret)
+const b = ret.join('')
+console.log(b)
+
+document.getElementById()
